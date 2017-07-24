6cd76fb867941a4c0a39bd366aaed35e5bcb41bd
GoldAdventurer <gpf.cd@hotmail.com>
2017-07-24 11:12:53 +0000

    fix all facebook url

--- __init__.py
+++ __init__.py
@@ -129,8 +129,8 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px; border-radius: 150px;\
-            -webkit-border-radius: 150px; -moz-border-radius: 150px;"> '
+    output += (' " style = "width: 300px; height: 300px; border-radius: 150px;'
+            '-webkit-border-radius: 150px; -moz-border-radius: 150px;"> ')
     flash("You are logged in as %s." % login_session['username'])
     return output
 
@@ -168,65 +168,31 @@ def gdisconnect():
 
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
-    print 'fb'
     # to protect against cross-state forgery attacks
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
-    print('access_token:'+ access_token)
     # Exchange client token for long-lived server-side token with GET /oauth/
     # access_token?grant_type=fb_exchange_token&client_id={app-id}&client_secret=
     # {app-secret}&fb_exchange_token={short-lived-token}
-    # app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']
-    # ['app_id']
-    # app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']
-    # ['app_secret']
     app_id = json.loads(open('/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())['web']\
     ['app_id']
-    print('app_id: ' + app_id)
-    # redirect_uri = json.loads(open('fb_client_secrets.json', 'r').
-    #                          read())['web']['redirect_uri']
-
-    # redirect_uri = json.loads(open('/var/www/catalog/catalog/fb_client_secrets.json', 'r').
-    #                          read())['web']['redirect_uri']
-
     app_secret = json.loads(
         open('/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())['web']['app_secret']
-    print('app_secret: '+app_secret)
-    #url = 'https://graph.facebook.com/oauth/access_token?grant_type=\
-    #    fb_exchange_token&client_id=%s&clientsecret=%s&fb_exchange_token=%s&\
-    #    redirect_uri=%s' \
-    #    % (app_id, app_secret, access_token, redirect_uri)
-
-    url = ('https://graph.facebook.com/v2.8/oauth/access_token?grant_type='
-        'fb_exchange_token&client_id=%s&clientsecret=%s&fb_exchange_token=%s') % (app_id, 
+    url = ('https://graph.facebook.com/v2.9/oauth/access_token?grant_type='
+        'fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s') % (app_id, 
         app_secret, access_token)
-
-    print ('url facebook: ' + url)
-    #url = ('https://graph.facebook.com/v2.9/oauth/access_token?'
-    #       'grant_type=fb_exchange_token&client_id=%s&client_secret=%s'
-    #       '&fb_exchange_token=%s') % (app_id, app_secret, access_token)
-
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print('result is :' + result)
     data = json.loads(result)
-    print('data is :')
-    print data
-
     # use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.9/me"
-    # strip expire tag from access token
-    ###token = result.split("&")[0]
     token = 'access_token=' + data['access_token']
-
-    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    url = 'https://graph.facebook.com/v2.9/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"%url
-    # print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
@@ -234,8 +200,8 @@ def fbconnect():
     login_session['facebook_id'] = data["id"]
 
     # get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&\
-        height=200&width=200' % token
+    url = ('https://graph.facebook.com/v2.9/me/picture?%s&redirect=0&'
+        'height=200&width=200') % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -255,8 +221,8 @@ def fbconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px; border-radius: 150px;\
-           -webkit-border-radius: 150px; -moz-border-radius: 150px;"> '
+    output += (' " style = width: 300px; height: 300px; border-radius: 150px;'
+           '-webkit-border-radius: 150px; -moz-border-radius: 150px;"> ')
     flash("you are logged in as %s" % login_session['username'])
     return output
 
